1c2d7286112c3b9026b3ca3fd8b9f8b39520b151
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-06 16:58:03 -0700

    We can now grab the user profile information using the callback function and pasport

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -9,7 +9,14 @@ passport.use(
         // client id and client secret
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }, () => {
+    }, (accessToken, refreshToken, profile, done) => {
+        //accessToken is a token we receive from google to read user data
+        //refresh token refreshes the access token
+        //profile is the information that passport comes back with when it takes the code to google and brings back the profile information
+        //done is called when we are done with this callback funciton
+
         // passport callback function
+        console.log('passport callback function fired')
+        console.log(profile)
     })
 )
