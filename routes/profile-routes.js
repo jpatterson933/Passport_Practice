60c503cfb9b53b26cb08d32f294da39e4c6c4c0d
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-07 01:43:44 -0700

    we are now rendering another html page using ejs called profile.ejs which the user will be routed to when they login.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 
 // middleware: a function that checks if the username is logged in and if they are not logged in it wont run any code that is below it
 const authCheck = (req, res, next) => {
-    if(!req.user){
+    if (!req.user) {
         // if user is not logged in
         res.redirect('/auth/login');
     } else {
         // if user is logged in - next means goes onto the next peice of middle ware
         next();
     }
-
 }
 
 router.get('/', authCheck, (req, res) => {
-    res.send('you are logged in, this is your profile - ' + req.user.username)
+    // user can be anything - and we are making user the req.user property which we have access to
+    res.render('profile', {user: req.user});
 });
 
 module.exports = router;
